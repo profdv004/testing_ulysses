d7d1ef6cc04d0bfd7418e2799b65960e2996a60c
Dhruv Pasari <dpasari@ip3-ws2.phyip3.dur.ac.uk>
2025-12-05 15:34:18 +0000

    testing

--- odeintw/__init__.py
+++ odeintw/__init__.py
@@ -2,11 +2,11 @@
 # All rights reserved.
 # See the LICENSE file for license information.
 
-from numpy.testing import Tester as _Tester
+# from numpy.testing import Tester as _Tester
 
 from ._odeintw import odeintw
 
 
 __version__ = "0.0.2.dev1"
 
-test = _Tester().test
+# test = _Tester().test
